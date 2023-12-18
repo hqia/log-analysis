4539107b39b2767eee2b9b6ef0f33bf38e541222
hqia <hqia@sina.com>
2023-12-18 14:00:04 +0800

    通过version选择输出相关配置

--- 123.py
+++ 123.py
@@ -4,6 +4,6 @@ VS写的第一个Python程序
 '''
 
 if __name__=="__main__":
-    name = input("What\'s your name?\n")
-    print("Hello %s"%name)
-    input("按任意键结束")
+    s = '10,2'
+    n=int(s)
+    print(n)
--- Log_analysis.py
+++ Log_analysis.py
@@ -9,14 +9,21 @@ list_audio_configs = []
 string_version = '' 
 audio_config_len = {'JL701n':13,'ac703n':12,'JL703n':13,}
 
+# #定义chip类，包含型号版本、音频配置长度、音频配置信息
+# class Chip(object):
+#     def __init__(self,version,audio_config_len,audio_config_date):
+#         self.version = version
+#         self.audio_config_len = audio_config_len
+#         self.audio_config_date = audio_config_date
+
 # 
 def jl701n_audio_config(lists):
     ADC_Bitwidth = lists[0].split(",")[0].split("Width:")[1]
     ADC_DCC = lists[0].split("DCC:")[1]
-    
+   
     ADC_MAX_Gain = lists[1].split(")")[0].split("Max:")[1]
     ADC_Gain = lists[1].split("):")[1].split(",6dB")[0]
-    ADC_6dB_Boost = lists[1].split("st:")[1].split(",")[0:4]
+    ADC_6dB_Boost = lists[1].split("st:")[1]
     
     DAC_Bitwidth = lists[2].split(",")[0].split("Width:")[1]
     DAC_DCC = lists[2].split("DCC:")[1]
@@ -47,13 +54,13 @@ def jl701n_audio_config(lists):
     DAA_CON7 = lists[11].split(" 7:")[1]
     
     ADA_CON0 = lists[12].split("CON 0:")[1].split("\t1")[0]
-    ADA_CON1 = lists[12].split("\t1")[1].split("\t2")[0]
-    ADA_CON2 = lists[12].split("\t2")[1].split("\t3")[0]
-    ADA_CON3 = lists[12].split("\t3")[1].split("\t4")[0]
-    ADA_CON4 = lists[12].split("\t4")[1].split("\t5")[0]
-    ADA_CON5 = lists[12].split("\t5")[1].split("\t6")[0]
-    ADA_CON6 = lists[12].split("\t6")[1].split("\t7")[0]
-    ADA_CON7 = lists[12].split("\t7")[1].split("\t8")[0]
+    ADA_CON1 = lists[12].split("\t1:")[1].split("\t2")[0]
+    ADA_CON2 = lists[12].split("\t2:")[1].split("\t3")[0]
+    ADA_CON3 = lists[12].split("\t3:")[1].split("\t4")[0]
+    ADA_CON4 = lists[12].split("\t4:")[1].split("\t5")[0]
+    ADA_CON5 = lists[12].split("\t5:")[1].split("\t6")[0]
+    ADA_CON6 = lists[12].split("\t6:")[1].split("\t7")[0]
+    ADA_CON7 = lists[12].split("\t7:")[1].split("\t8")[0]
     ADA_CON8 = lists[12].split("\t8:")[1]
     
     print("ADC位宽=" + ADC_Bitwidth)
@@ -62,7 +69,65 @@ def jl701n_audio_config(lists):
     print("DAC位宽=" + DAC_Bitwidth)
     print(DAC_Max_AGain)  
 # 
-#audio_config_print = {'JL701n':jl701n_audio_config(list_audio_configs),'ac703n':12,'JL703n':13,}
+def ac703n_audio_config(lists):
+    ADC_CFG_PGA = lists[0].split("PGA:")[1].split(",6dB")[0]
+    ADC_CFG_6dB_Boost = lists[0].split("st:")[1]
+    
+    DAC_CFG_AGain = lists[1].split("AGain:")[1].split(",DGain")[0]
+
+    DAC_CFG_DGain = lists[1].split("DGain:")[1]
+    
+    ADC_Bitwidth = lists[2].split("ADC = ")[1]
+    DAC_Bitwidth = lists[2].split("DAC = ")[1].split(",ADC")[0]
+    
+    JL_WL_AUD_CON0 = lists[3].split("CON0:")[1]
+    
+    AUD_CON = lists[4].split("CON:")[1]
+    
+    DAC_CON = lists[5].split("CON:")[1]
+    
+    ADC_CON = lists[6].split("CON:")[1]
+    
+    ADC_CON1 = lists[7].split("CON1:")[1]
+    
+    DAC_TM0 = lists[8].split("TM0:")[1]
+    
+    DAA_CON0 = lists[9].split("CON 0:")[1].split(" ")[0]
+    DAA_CON1 = lists[9].split("\t1:")[1].split(",\t2:")[0]
+    DAA_CON2 = lists[9].split("\t2:")[1].split(",\t3")[0]
+    DAA_CON3 = lists[9].split("\t3:")[1].split("    ")[0]
+    DAA_CON4 = lists[9].split("    4:")[1].split(",7:")[0]
+    DAA_CON7 = lists[9].split(",7:")[1]
+    
+    ADA_CON0 = lists[10].split("CON 0:")[1].split("\t1")[0]
+    ADA_CON1 = lists[10].split("\t1:")[1].split("\t2")[0]
+    ADA_CON2 = lists[10].split("\t2:")[1].split("\t3")[0]
+    ADA_CON3 = lists[10].split("\t3:")[1].split("\t4")[0]
+    ADA_CON4 = lists[10].split("\t4:")[1].split("\t5")[0]
+    ADA_CON5 = lists[10].split("\t5:")[1].split("  6:")[0]
+    ADA_CON6 = lists[10].split("  6:")[1].split(" 7:")[0]
+    ADA_CON7 = lists[10].split(" 7:")[1].split(" 8:")[0]
+    ADA_CON8 = lists[10].split(" 8:")[1]
+    
+    ADDA_CON0 = lists[11].split("CON 0:")[1].split("\t1:")[0]
+    ADDA_CON1 = lists[11].split("\t1:")[1]
+    
+    print("ADC位宽=" + ADC_Bitwidth)
+
+    print("-------ac703--------")
+    
+#
+
+def show_config(chip_version):
+    if '701' in chip_version:
+        jl701n_audio_config(list_audio_configs)
+        return
+    elif '703' in chip_version:
+        ac703n_audio_config(list_audio_configs)
+        return
+    else:
+        print("目前还不知道%s的配置信息"%chip_version)
+    
 
 with open('log.txt','r')as f:
     lines = f.readlines()
@@ -80,13 +145,14 @@ with open('log.txt','r')as f:
             print('第' + str(count) + '行')
             continue
         if audio_config_begin:
-            if get_len == len_max:
+            if get_len >= len_max:
                 get_len = 0
                 list_audio_configs.clear()
             list_audio_configs.append(line.split('\n')[0])
+            
             get_len += 1
             if(get_len == len_max):
                 audio_config_begin = False
 
-# audio_config_print.get(string_version)
-jl701n_audio_config(list_audio_configs)
+
+show_config(string_version)
